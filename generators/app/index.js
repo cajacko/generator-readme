a7c161baf6a81d8b252ffb84e52e1f2d9f05bf5a
Charlie Jackson <charlie@charliejackson.com>
2017-03-21 10:04:46 +0000

    Added readme file logic

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,3 +1,5 @@
+/* @flow */
+
 const Generator = require('yeoman-generator');
 const path = require('path');
 const checkOutOfDatePackages = require('check-out-of-date-packages');
@@ -9,16 +11,41 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copyTpl(
-      this.templatePath('./**/*'),
-      this.destinationPath('./'),
-      { variable: 'value' }
-    );
+    let readme = '';
+    const destPath = this.destinationPath('./README.md');
 
-    this.fs.copyTpl(
-      this.templatePath('./**/.*'),
-      this.destinationPath('./'),
-      { variable: 'value' }
-    );
+    if (this.options && this.options.tag && this.options.markdown) {
+      const start = `<!--- generator-readme-start ${this.options.tag} -->`;
+      const end = `<!--- generator-readme-end ${this.options.tag} -->`;
+      const partial = `${start}\n${this.options.markdown}\n${end}`;
+
+      if (this.fs.exists(destPath)) {
+        const file = this.fs.read(destPath);
+        const replace = `^${start}[\\s\\S]*^${end}`;
+        const re = new RegExp(replace, 'm');
+
+        readme = file.replace(re, partial);
+      } else {
+        readme = partial;
+      }
+
+      return this.fs.write(destPath, readme);
+    }
+
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Readme Title: ',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Readme Description: ',
+      }
+    ]).then((answers) => {
+      readme = `# ${answers.title}\n\n${answers.description}`;
+      return this.fs.write(destPath, readme);
+    });
   }
 };
